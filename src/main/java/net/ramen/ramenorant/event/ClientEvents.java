34732bdb9b4ddcff15ae259baa388a1aa08f31f8
Ramen5914 <Ramen5914@gmail.com>
2023-05-21 20:52:12 -0700

    Added all key binds

--- src/main/java/net/ramen/ramenorant/event/ClientEvents.java
+++ src/main/java/net/ramen/ramenorant/event/ClientEvents.java
@@ -1,5 +1,7 @@
 package net.ramen.ramenorant.event;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
@@ -12,15 +14,86 @@ public class ClientEvents {
     @Mod.EventBusSubscriber(modid = Ramenorant.MOD_ID, value = Dist.CLIENT)
     public static class ClientForgeEvents {
         @SubscribeEvent
-        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
-            event.register(KeyBinding.RELOAD_KEY);
-        }
-
-        @SubscribeEvent
         public static void onKeyInput(InputEvent.Key event) {
-            if (KeyBinding.RELOAD_KEY.consumeClick()) {
-                
+            if (KeyBinding.ABILITY_1_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ability 1."));
+            } else if (KeyBinding.ABILITY_2_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ability 2."));
+            } else if (KeyBinding.ABILITY_3_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ability 3"));
+            } else if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
             }
+
+            if (KeyBinding.RELOAD_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Reloaded Weapon."));
+            }
+            if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
+            }
+            if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
+            }
+            if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
+            }
+            if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
+            }
+            if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
+            }
+            if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
+            }
+            if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
+            }
+            if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
+            }
+            if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
+            }
+            if (KeyBinding.ABILITY_ULT_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Used Ultimate Ability"));
+            }
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = Ramenorant.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
+            // Abilities
+            event.register(KeyBinding.ABILITY_1_KEY);
+            event.register(KeyBinding.ABILITY_2_KEY);
+            event.register(KeyBinding.ABILITY_3_KEY);
+            event.register(KeyBinding.ABILITY_ULT_KEY);
+
+            // Weapons
+            event.register(KeyBinding.RELOAD_KEY);
+
+            // Interaction
+            event.register(KeyBinding.INSPECT_KEY);
+            event.register(KeyBinding.USE_OBJECT);
+            event.register(KeyBinding.USE_SPIKE);
+
+            // Pings
+            event.register(KeyBinding.PING);
+
+            // General
+            event.register(KeyBinding.SHOW_LOADOUTS);
+            event.register(KeyBinding.OPEN_ARMORY);
+            event.register(KeyBinding.OPEN_MAP_TOGGLE);
+            event.register(KeyBinding.OPEN_MAP_HOLD);
+            event.register(KeyBinding.SHOW_SCOREBOARD);
+
+            // Voting
+            event.register(KeyBinding.VOTE_OPTION_1);
+            event.register(KeyBinding.VOTE_OPTION_2);
+            event.register(KeyBinding.VOTE_OPTION_3);
+            event.register(KeyBinding.VOTE_OPTION_4);
         }
     }
 }
--- src/main/java/net/ramen/ramenorant/util/KeyBinding.java
+++ src/main/java/net/ramen/ramenorant/util/KeyBinding.java
@@ -6,21 +6,58 @@ import net.minecraftforge.client.settings.KeyConflictContext;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyBinding {
-    public static final String RAMENORANT_KEY_CATEGORY = "key.categories.ramenorant.ramenorant";
+    public static final String RAMENORANT_ABILITIES_KEY_CATEGORY = "key.category.ramenorant.abilities";
+    public static final String RAMENORANT_WEAPONS_KEY_CATEGORY = "key.category.ramenorant.weapons";
+    public static final String RAMENORANT_INTERACTION_KEY_CATEGORY = "key.category.ramenorant.interaction";
+    public static final String RAMENORANT_PINGS_KEY_CATEGORY = "key.category.ramenorant.pings";
+    public static final String RAMENORANT_GENERAL_KEY_CATEGORY = "key.category.ramenorant.general";
+    public static final String RAMENORANT_VOTING_KEY_CATEGORY = "key.category.ramenorant.voting";
 
-    public static final KeyMapping RELOAD_KEY = new KeyMapping(
-            "key.ramenorant.reload",
-            KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM,
-            GLFW.GLFW_KEY_R,
-            RAMENORANT_KEY_CATEGORY
-    );
+    // Abilities:
+    public static final KeyMapping ABILITY_1_KEY = new KeyMapping("key.ramenorant.ability_1", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_C, RAMENORANT_ABILITIES_KEY_CATEGORY);
+    public static final KeyMapping ABILITY_2_KEY = new KeyMapping("key.ramenorant.ability_2", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Q, RAMENORANT_ABILITIES_KEY_CATEGORY);
+    public static final KeyMapping ABILITY_3_KEY = new KeyMapping("key.ramenorant.ability_3", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_E, RAMENORANT_ABILITIES_KEY_CATEGORY);
+    public static final KeyMapping ABILITY_ULT_KEY = new KeyMapping("key.ramenorant.ability_ult", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_X, RAMENORANT_ABILITIES_KEY_CATEGORY);
 
-    public static final KeyMapping INSPECT_KEY = new KeyMapping(
-        "key.ramenorant.inspect",
-        KeyConflictContext.IN_GAME,
-        InputConstants.Type.KEYSYM,
-        GLFW.GLFW_KEY_Y,
-        RAMENORANT_KEY_CATEGORY
-    );
+    // Weapons:
+    public static final KeyMapping RELOAD_KEY = new KeyMapping("key.ramenorant.reload", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R, RAMENORANT_WEAPONS_KEY_CATEGORY);
+
+    // Interaction:
+    public static final KeyMapping INSPECT_KEY = new KeyMapping("key.ramenorant.inspect", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Y, RAMENORANT_INTERACTION_KEY_CATEGORY);
+    public static final KeyMapping USE_OBJECT = new KeyMapping("key.ramenorant.use_object", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_F, RAMENORANT_INTERACTION_KEY_CATEGORY);
+    public static final KeyMapping USE_SPIKE = new KeyMapping("key.ramenorant.use_spike", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_4, RAMENORANT_INTERACTION_KEY_CATEGORY);
+
+    // Pings:
+    public static final KeyMapping PING = new KeyMapping("key.ramenorant.ping", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z, RAMENORANT_PINGS_KEY_CATEGORY);
+
+    // General:
+    public static final KeyMapping SHOW_LOADOUTS = new KeyMapping("key.ramenorant.show_loadouts", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_ALT, RAMENORANT_GENERAL_KEY_CATEGORY);
+    public static final KeyMapping OPEN_ARMORY = new KeyMapping("key.ramenorant.open_armory", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_B, RAMENORANT_GENERAL_KEY_CATEGORY);
+    public static final KeyMapping OPEN_MAP_TOGGLE = new KeyMapping("key.ramenorant.open_map_toggle", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_M, RAMENORANT_GENERAL_KEY_CATEGORY);
+    public static final KeyMapping OPEN_MAP_HOLD = new KeyMapping("key.ramenorant.open_map_hold", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_CAPS_LOCK, RAMENORANT_GENERAL_KEY_CATEGORY);
+    public static final KeyMapping SHOW_SCOREBOARD = new KeyMapping("key.ramenorant.show_scoreboard", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_TAB, RAMENORANT_GENERAL_KEY_CATEGORY);
+
+    // Voting:
+    public static final KeyMapping VOTE_OPTION_1 = new KeyMapping("key.ramenorant.vote_option_1", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_F5, RAMENORANT_VOTING_KEY_CATEGORY);
+    public static final KeyMapping VOTE_OPTION_2 = new KeyMapping("key.ramenorant.vote_option_2", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_F6, RAMENORANT_VOTING_KEY_CATEGORY);
+    public static final KeyMapping VOTE_OPTION_3 = new KeyMapping("key.ramenorant.vote_option_3", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_F7, RAMENORANT_VOTING_KEY_CATEGORY);
+    public static final KeyMapping VOTE_OPTION_4 = new KeyMapping("key.ramenorant.vote_option_4", KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_F8, RAMENORANT_VOTING_KEY_CATEGORY);
 }
