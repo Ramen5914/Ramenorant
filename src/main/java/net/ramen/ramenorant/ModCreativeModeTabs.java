ee64d304be53d462cfa9702d36e74d349f763761
Ramen5914 <Ramen5914@gmail.com>
2023-06-11 02:21:05 -0700

    fixed creative mode tabs

--- src/main/java/net/ramen/ramenorant/ModCreativeModeTabs.java
+++ src/main/java/net/ramen/ramenorant/ModCreativeModeTabs.java
@@ -1,33 +1,31 @@
 package net.ramen.ramenorant;
 
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.event.CreativeModeTabEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.ramen.ramenorant.item.ModAbilities;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.ramen.ramenorant.item.ModGuns;
 
-@Mod.EventBusSubscriber(modid = Ramenorant.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModCreativeModeTabs {
-    public static CreativeModeTab RAMENORANT_TAB;
-    public static CreativeModeTab RAMENORANT_GUNS_TAB;
-    public static CreativeModeTab RAMENORANT_ABILITIES_TAB;
+    private static final DeferredRegister<CreativeModeTab> RAMENORANT_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Ramenorant.MOD_ID);
 
-    @SubscribeEvent
-    public static void registerCreativeModeTabs(CreativeModeTabEvent.Register event) {
-        RAMENORANT_TAB = event.registerCreativeModeTab(new ResourceLocation(Ramenorant.MOD_ID, "ramenorant_tab"),
-                builder -> builder.icon(() -> new ItemStack(ModGuns.BLACK_OPAL.get()))
-                        .title(Component.translatable("creativemodetab.ramenorant_tab")));
+    public static final RegistryObject<CreativeModeTab> RAMENORANT_TAB = RAMENORANT_TABS.register("ramenorant_tab", () -> {
+        return CreativeModeTab.builder()
+                .icon(() -> new ItemStack(ModGuns.BLACK_OPAL.get()))
+                .displayItems((features, output) -> {
+                    output.accept(new ItemStack(ModGuns.BLACK_OPAL.get()));
+                    output.accept(new ItemStack(ModGuns.RAW_BLACK_OPAL.get()));
+                })
+                .title(Component.translatable("itemgroup.ramenorant_tab"))
+                .build();
+    });
 
-        RAMENORANT_GUNS_TAB = event.registerCreativeModeTab(new ResourceLocation(Ramenorant.MOD_ID, "ramenorant_guns_tab"),
-                builder -> builder.icon(() -> new ItemStack(ModGuns.CLASSIC.get()))
-                        .title(Component.translatable("creativemodetab.ramenorant_guns_tab")));
-
-        RAMENORANT_ABILITIES_TAB = event.registerCreativeModeTab(new ResourceLocation(Ramenorant.MOD_ID, "ramenorant_abilities_tab"),
-                builder -> builder.icon(() -> new ItemStack(ModAbilities.LEER.get()))
-                        .title(Component.translatable("creativemodetab.ramenorant_abilities_tab")));
+    public static void register(IEventBus eventBus) {
+        RAMENORANT_TABS.register(eventBus);
     }
 }
